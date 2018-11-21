e549aed00c758454fdede7a80ff2061367b3ff66
Mikhail Belov <mbelov@os-company.ru>
2018-11-21 18:49:38 +0500

    init

---
+++ TPRLab5/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace TPRLab5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.dgvEndResult = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvCrits = new System.Windows.Forms.DataGridView();
+            this.dgvInput = new System.Windows.Forms.DataGridView();
+            this.Name1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nuCriteries = new System.Windows.Forms.NumericUpDown();
+            this.nuAlternatives = new System.Windows.Forms.NumericUpDown();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEndResult)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCrits)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInput)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nuCriteries)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nuAlternatives)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(27, 34);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(151, 27);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Расчёт";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // dgvEndResult
+            // 
+            this.dgvEndResult.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dgvEndResult.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEndResult.Location = new System.Drawing.Point(27, 76);
+            this.dgvEndResult.Name = "dgvEndResult";
+            this.dgvEndResult.Size = new System.Drawing.Size(349, 111);
+            this.dgvEndResult.TabIndex = 8;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnLoad);
+            this.groupBox1.Controls.Add(this.btnSave);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.dgvCrits);
+            this.groupBox1.Controls.Add(this.dgvInput);
+            this.groupBox1.Controls.Add(this.nuCriteries);
+            this.groupBox1.Controls.Add(this.nuAlternatives);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(436, 437);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Входные данные";
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(213, 391);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(147, 23);
+            this.btnLoad.TabIndex = 19;
+            this.btnLoad.Text = "Загрузить из файла";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(67, 391);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(133, 23);
+            this.btnSave.TabIndex = 18;
+            this.btnSave.Text = "Сохранить в файл";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 13);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Кол-во альтернатив";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(178, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 13);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Кол-во критериев";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 265);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Веса критериев";
+            // 
+            // dgvCrits
+            // 
+            this.dgvCrits.AllowUserToAddRows = false;
+            this.dgvCrits.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dgvCrits.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgvCrits.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.Raised;
+            this.dgvCrits.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCrits.Location = new System.Drawing.Point(17, 281);
+            this.dgvCrits.Name = "dgvCrits";
+            this.dgvCrits.Size = new System.Drawing.Size(398, 95);
+            this.dgvCrits.TabIndex = 14;
+            // 
+            // dgvInput
+            // 
+            this.dgvInput.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dgvInput.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgvInput.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.Raised;
+            this.dgvInput.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvInput.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Name1});
+            this.dgvInput.Location = new System.Drawing.Point(17, 88);
+            this.dgvInput.Name = "dgvInput";
+            this.dgvInput.Size = new System.Drawing.Size(398, 165);
+            this.dgvInput.TabIndex = 13;
+            // 
+            // Name1
+            // 
+            this.Name1.HeaderText = "Название";
+            this.Name1.Name = "Name1";
+            // 
+            // nuCriteries
+            // 
+            this.nuCriteries.Location = new System.Drawing.Point(181, 50);
+            this.nuCriteries.Name = "nuCriteries";
+            this.nuCriteries.Size = new System.Drawing.Size(162, 20);
+            this.nuCriteries.TabIndex = 12;
+            // 
+            // nuAlternatives
+            // 
+            this.nuAlternatives.Location = new System.Drawing.Point(17, 50);
+            this.nuAlternatives.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nuAlternatives.Name = "nuAlternatives";
+            this.nuAlternatives.Size = new System.Drawing.Size(133, 20);
+            this.nuAlternatives.TabIndex = 11;
+            this.nuAlternatives.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dgvEndResult);
+            this.groupBox2.Controls.Add(this.btnStart);
+            this.groupBox2.Location = new System.Drawing.Point(454, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(403, 414);
+            this.groupBox2.TabIndex = 12;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(932, 495);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEndResult)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCrits)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInput)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nuCriteries)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nuAlternatives)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.DataGridView dgvEndResult;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvCrits;
+        private System.Windows.Forms.DataGridView dgvInput;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Name1;
+        private System.Windows.Forms.NumericUpDown nuCriteries;
+        private System.Windows.Forms.NumericUpDown nuAlternatives;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ TPRLab5/Form1.cs
@@ -0,0 +1,396 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TPRLab5
+{
+
+    public delegate double P(double d);
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            CritPs = new List<PFunc>();
+            InitializeComponent();
+
+        }
+
+        private void nuAlternatives_ValueChanged(object sender, EventArgs e)
+        {
+            dgvInput.RowCount = (int)nuAlternatives.Value;
+            dgvInput.Rows[dgvInput.RowCount - 1].HeaderCell.Value = ("a" + dgvInput.RowCount);
+            //  Input();
+        }
+
+
+        private void nuCriteries_ValueChanged(object sender, EventArgs e)
+        {
+            dgvInput.ColumnCount = (int)nuCriteries.Value + 1;
+            dgvCrits.ColumnCount = (int)nuCriteries.Value;
+            dgvCrits.RowCount = 1;
+            if ((int)nuCriteries.Value > 0)
+            {
+                dgvCrits.Columns[dgvCrits.ColumnCount - 1].HeaderText = "w" + dgvCrits.ColumnCount;
+                dgvInput.Columns[dgvInput.ColumnCount - 1].HeaderText = "f" + (dgvInput.ColumnCount - 1);
+            }
+            // Input();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dgvInput.ColumnCount = (int)nuCriteries.Value + 1;
+            dgvInput.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            dgvInput.RowCount = (int)nuAlternatives.Value;
+            dgvInput.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            dgvInput.AllowUserToAddRows = false;
+            dgvInput.RowCount = 1;
+            dgvInput.Rows[dgvInput.RowCount - 1].HeaderCell.Value = ("a" + dgvInput.RowCount);
+            if (dgvInput.ColumnCount > 1)
+                dgvInput.Columns[dgvInput.ColumnCount - 1].HeaderText = "f" + (dgvInput.ColumnCount - 1);
+            dgvInput.RowHeadersWidthSizeMode = DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+
+
+          
+           dgvCrits.ColumnCount = (int)nuCriteries.Value;
+            dgvCrits.RowCount = 1;
+            dgvCrits.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            dgvCrits.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            dgvCrits.Columns[dgvCrits.ColumnCount - 1].HeaderText = "w" + dgvCrits.ColumnCount;
+
+        }
+
+        string[] altNames;
+        int crits;
+        int alts;
+        Matrix critMat;
+        Matrix[] d;
+        double[] w;
+        void Input()
+        {
+            crits = (int)nuCriteries.Value;
+            alts = (int)nuAlternatives.Value;
+            altNames = new string[alts];
+            for (int i = 0; i < alts && i < dgvInput.RowCount; i++)
+                if (dgvInput[0, i] != null && dgvInput[0, i].Value != null)
+                    altNames[i] = dgvInput[0, i].Value.ToString();
+                else
+                    altNames[i] = "не задано";
+
+            critMat = new Matrix(alts, crits);
+            for (int i = 0; i < alts && i < dgvInput.RowCount; i++)
+                for (int j = 0; j < crits && j + 1 < dgvInput.ColumnCount; j++)
+                {
+                    if (dgvInput[j + 1, i] != null && dgvInput[j + 1, i].Value != null &&
+                           double.TryParse(dgvInput[j + 1, i].Value.ToString().Replace('.', ','), out double d))
+                        critMat[i, j] = d;
+                    else
+                        critMat[i, j] = 0;
+                }
+
+            w = new double[crits];
+            for (int crit = 0; crit < crits && crit < dgvCrits.ColumnCount; crit++)
+                if (dgvCrits[crit, 0] != null && dgvCrits[crit, 0].Value != null
+                    && double.TryParse(dgvCrits[crit, 0].Value.ToString().Replace('.', ','), out double d))
+                    w[crit] = d;
+                else
+                    w[crit] = 0;
+
+            if (CritPs.Count > crits)
+                CritPs.RemoveAt(CritPs.Count - 1);
+            if (CritPs.Count < crits)
+            {
+                InputPForm pf = new InputPForm();
+                pf.ShowDialog();
+                CritPs.Add(pf.result);
+            }
+
+
+        }
+        double[] F_min, F_plus, F;
+        Matrix pi;
+        Matrix mat2;
+        List<PFunc> CritPs;
+        void Calculate()
+        {
+
+            d = new Matrix[crits];
+            for (int c = 0; c < crits; c++)
+            {
+                Matrix mat = new Matrix(alts, alts);
+                for (int i = 0; i < alts; i++)
+                    for (int j = 0; j < alts; j++)
+                        mat[i, j] = critMat[i, c] - critMat[j, c];
+
+
+                d[c] = mat;
+            }
+
+            mat2 = new Matrix(alts * crits, alts);
+
+            for (int alt1 = 0; alt1 < alts; alt1++)
+                for (int alts2 = 0; alts2 < alts; alts2++)
+                {
+                    for (int crit = 0; crit < crits; crit++)
+                        mat2[crit * alts + alt1, alts2] = CritPs[crit].func(d[crit][alt1, alts2]);
+                }
+
+
+            pi = new Matrix(alts, alts);
+
+            for (int i = 0; i < alts; i++)
+                for (int j = 0; j < alts; j++)
+                {
+                    double sum = 0;
+                    for (int crit = 0; crit < crits; crit++)
+                        sum += w[crit] * CritPs[crit].func(d[crit][i, j]);
+                    pi[i, j] = sum;
+                }
+
+
+            F_min = new double[alts]; F_plus = new double[alts]; F = new double[alts];
+
+            for (int i = 0; i < alts; i++)
+            {
+                F_min[i] = 0;
+                F_plus[i] = 0;
+                for (int j = 0; j < alts; j++)
+                    F_min[i] += pi[j, i];
+                for (int j = 0; j < alts; j++)
+                    F_plus[i] += pi[i, j];
+                F[i] = F_plus[i] - F_min[i];
+            }
+
+
+            F_alts = new SortedDictionary<double, string>();
+            for (int i = 0; i < alts; i++)
+                F_alts.Add(F[i], altNames[i]);
+
+        }
+        SortedDictionary<double, string> F_alts;
+        DataGridView newDGV(int x, int y, int cols, int rows)
+        {
+            var dgv = new DataGridView();
+            dgv.Top = y;
+            dgv.Left = x;
+
+            dgv.ColumnCount = cols;
+            dgv.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            dgv.RowHeadersWidthSizeMode = DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            dgv.RowCount = rows;
+            return dgv;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            Input();
+            SaveFileDialog sfd = new SaveFileDialog();
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter sw = new StreamWriter(sfd.FileName);
+
+                sw.WriteLine(alts);
+                sw.WriteLine(crits);
+
+                for (int i = 0; i < alts; i++)
+                    sw.WriteLine(altNames[i]);
+
+                critMat.Save(sw);
+
+                for (int i = 0; i < crits; i++)
+                    sw.WriteLine(w[i]);
+
+                for (int i = 0; i < crits; i++)
+                    CritPs[i].Save(sw);
+
+
+                sw.Close();
+            }
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            FileDialog sfd = new OpenFileDialog();
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader sw = new StreamReader(sfd.FileName);
+
+                int alts = int.Parse(sw.ReadLine());
+                int crits = int.Parse(sw.ReadLine());
+                var altNames = new string[alts];
+                for (int i = 0; i < alts; i++)
+                    altNames[i] = sw.ReadLine();
+
+                var critMat = new Matrix(alts, crits);
+                critMat.Load(sw);
+
+                var w = new double[crits];
+                for (int i = 0; i < crits; i++)
+                {
+                    w[i] = double.Parse(sw.ReadLine());
+                }
+
+                CritPs.Clear();
+                for (int i = 0; i < crits; i++)
+                {
+                    CritPs.Add(PFunc.Load(sw));
+                }
+
+                sw.Close();
+
+                nuAlternatives.Value = alts;
+                nuCriteries.Value = crits;
+
+                for (int i = 0; i < crits; i++)
+                    dgvCrits[i, 0].Value = w[i];
+
+                for (int i = 0; i < alts; i++)
+                    dgvInput[0, i].Value = altNames[i];
+
+                for (int i = 0; i < alts; i++)
+                    for (int j = 0; j < crits; j++)
+                        dgvInput[j + 1, i].Value = critMat[i, j];
+
+            }
+
+            Input();
+
+        }
+
+        private void dgvInput_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            //Input();
+        }
+
+        private void dgvCrits_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            // Input();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Input();
+                Calculate();
+                Output();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+        void Output()
+        {
+            int x = dgvInput.Left, y = dgvInput.Top + dgvInput.Height + 20;
+
+            DataGridView dgv = null;
+            //////////////////////////////////////
+            ///матрицы сравнений по критериям
+            /////////////////////////////////////
+            for (int c = 0; c < crits; c++)
+            {
+
+                dgv = newDGV(x, y, alts, alts);
+                dgv.TopLeftHeaderCell.Value = "Критерий" + (c + 1).ToString();
+
+                for (int i = 0; i < alts; i++)
+                {
+                    dgv.Rows[i].HeaderCell.Value = altNames[i];
+                    dgv.Columns[i].HeaderText = altNames[i];
+                }
+                for (int i = 0; i < alts; i++)
+                    for (int j = 0; j < alts; j++)
+                        dgv[j, i].Value = d[c][i, j];
+
+                this.Controls.Add(dgv);
+
+                x += dgv.Width + 50;
+            }
+            //////////////////////////////////
+            x = dgvInput.Left;
+            y += 50 + dgv.Height;
+
+            ///////////////////////////////////
+            ///матрицы с p 
+            /////////////////////
+            dgv = newDGV(x, y, alts + 1, crits * alts);
+
+            for (int i = 0; i < dgv.RowCount - 1; i++)
+                dgv.Rows[i + 1].HeaderCell.Value = "P" + (i / alts + 1).ToString();
+
+            for (int i = 0; i < dgv.RowCount - 1; i++)
+                dgv[0, i].Value = "a" + (i % alts + 1).ToString();
+
+            for (int i = 0; i < alts; i++)
+                dgv.Columns[i + 1].HeaderText = "a" + (i + 1).ToString();
+
+            for (int i = 0; i < mat2.n; i++)
+                for (int j = 0; j < mat2.m; j++)
+                    dgv[j + 1, i].Value = mat2[i, j];
+
+
+
+            this.Controls.Add(dgv);
+            this.AutoScroll = true;
+
+
+            //////////////////////////
+            x += dgv.Width + 20;
+
+
+            ////////////////////////
+            ///матрица пи и Ф+, Ф-
+            //////////////////////////////
+            dgv = newDGV(x, y, alts + 1, alts + 1);
+
+            for (int i = 0; i < alts; i++)
+                dgv.Columns[i].HeaderText = "pi(ai, a" + (i + 1).ToString() + ")";
+            dgv.Columns[alts].HeaderText = "Ф+";
+
+            for (int i = 0; i < alts; i++)
+                dgv.Rows[i].HeaderCell.Value = "pi(a" + (i + 1).ToString() + ", aj)";
+            dgv.Rows[alts].HeaderCell.Value = "Ф-";
+
+            for (int i = 0; i < alts; i++)
+                for (int j = 0; j < alts; j++)
+                    dgv[j, i].Value = pi[i, j];
+
+            for (int i = 0; i < alts; i++)
+                dgv[alts, i].Value = F_plus[i];
+
+            for (int i = 0; i < alts; i++)
+                dgv[i, alts].Value = F_min[i];
+
+            this.Controls.Add(dgv);
+
+            ////////////////////////////////
+            ///результаты
+            ///////////////////////////////
+
+            dgvEndResult.RowCount = 2;
+            dgvEndResult.ColumnCount = alts;
+            dgvEndResult.Rows[0].HeaderCell.Value = "a";
+            dgvEndResult.Rows[1].HeaderCell.Value = "F";
+
+            int iter = 0;
+            foreach (var par in F_alts.Reverse())
+            {
+                dgvEndResult[iter, 0].Value = par.Value;
+                dgvEndResult[iter, 1].Value = par.Key;
+                iter++;
+            }
+        }
+    }
+}
